b4c854e660171ab8d65eb9fe8f41b0f951f97a22
Del <del@delizardo.com>
2021-08-23 15:11:18 -0500

    wrote perfect numbers

---
+++ Challenges/PerfectNumbers/perfect-number.js
@@ -0,0 +1,27 @@
+class PerfectNumber {
+  constructor() {}
+
+  static classify(number) {
+    if (number < 0) throw new TypeError("Non-negative integers only");
+
+    let divisors = [1];
+
+    for (let val = 2; val < Math.ceil(Math.sqrt(number)); val++) {
+      debugger;
+      if (number % val === 0) {
+        divisors.push(val, number / val);
+      }
+    }
+
+    let aliquot = divisors.reduce((sum, num) => sum + num, 0);
+    if (aliquot === number) {
+      return 'perfect';
+    }
+
+    return (aliquot < number ? 'deficient' : 'abundant');
+  }
+}
+
+module.exports = { PerfectNumber };
+
+PerfectNumber.classify(28);
